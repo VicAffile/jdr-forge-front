9d413070f38d6ab5a5fdb9d9571a1763dd94d719
macsimtv <maximelecouturier@gmail.com>
2023-11-09 12:12:13 +0100

    🚀 Setup

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <header>
+    <div>
+      <RouterLink to="/">
+        Accueil
+      </RouterLink>
+      <RouterLink to="/about">
+        About
+      </RouterLink>
+    </div>
+  </header>
+</template>
